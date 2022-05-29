d211ec700b8e3f4a3ad7c2b4f81ebb8b0d2e990f
TodorAtanassov <todoratanasov432@gmail.com>
2022-05-29 13:27:18 +0300

    Rock paper scissors

--- rps.py
+++ rps.py
@@ -4,7 +4,7 @@ import random
 play = True
 c_score = 0
 u_score = 0
-choices = ['R', 'P', 'S']
+choices = ['r', 'p', 's']
 c_choice = random.choice(choices)
 while play:
     u_choice = input('''type "R" for Rock,
@@ -14,13 +14,19 @@ while play:
     if u_choice == 'stop':
         break
 print('Game is terminated!')
-if c_choice == 'R' and u_choice == 'r':
+if c_choice == 'r' and u_choice == 'r':
     print(f'''You:Rock -- AI: Rock
             DRAW
             You:{u_score} --- AI:{c_score}''')
-elif c_choice == 'S' and u_choice == 's':
+elif c_choice == 's' and u_choice == 's':
     print('''You:Scissors -- AI: Scissors
                 DRAW''')
-elif c_choice == 'P' and u_choice == 'p':
+elif c_choice == 'p' and u_choice == 'p':
     print('''You:Paper -- AI: Paper
              DRAW''')
+elif c_choice == 'p' and u_choice == 'r':
+    c_score += 1
+    print('''You:Paper -- AI: Paper
+             AI WINS''')
+
+
