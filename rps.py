27f91011b8989e4ce66e0e4d43c7d9f2025a4339
TodorAtanassov <todoratanasov432@gmail.com>
2022-05-31 08:48:11 +0300

    Rock paper scissors

--- rps.py
+++ rps.py
@@ -5,13 +5,14 @@ play = True
 c_score = 0
 u_score = 0
 choices = ['r', 'p', 's']
-c_choice = random.choice(choices)
+
 
 u_choice = input('''type "R" for Rock,
      "P" for Paper,
      "S" for scissors and
      "Stop" to end the game. \n''').lower()
 while play:
+    c_choice = random.choice(choices)
     u_choice = input('>')
     if u_choice == 'r' and c_choice == 'r':
         print(f'''You:Rock -- AI: Rock
@@ -19,34 +20,42 @@ while play:
                 You:{u_score} --- AI:{c_score}''')
     elif u_choice == 's' and c_choice == 's':
         print('''You:Scissors -- AI: Scissors
-                    DRAW''')
+                    DRAW
+                     You:{u_score} --- AI:{c_score}''')
     elif u_choice == 'p' and c_choice == 'p':
         print('''You:Paper -- AI: Paper
-                 DRAW''')
+                 DRAW
+                  You:{u_score} --- AI:{c_score}''')
     elif u_choice == 'r' and c_choice == 'p':
         c_score += 1
         print('''You: Rock -- AI: Paper
-                 AI WINS''')
+                 AI WINS
+                  You:{u_score} --- AI:{c_score}''')
     elif u_choice == 's' and c_choice == 'r':
         c_score += 1
         print('''You: Scissors -- AI: Rock
-                 AI WINS''')
+                 AI WINS
+                  You:{u_score} --- AI:{c_score}''')
     elif u_choice == 'p' and c_choice == 's':
         c_score += 1
         print('''You: Paper -- AI: Scissors
-                 AI WINS''')
+                 AI WINS
+                  You:{u_score} --- AI:{c_score}''')
     elif u_choice == 's' and c_choice == 'p':
         u_score += 1
         print('''You: Scissors -- AI: Paper
-                 YOU WIN''')
+                 YOU WIN
+                  You:{u_score} --- AI:{c_score}''')
     elif u_choice == 'p' and c_choice == 'r':
         u_score += 1
         print('''You: Paper -- AI: Rock
-                 YOU WIN''')
+                 YOU WIN
+                  You:{u_score} --- AI:{c_score}''')
     elif u_choice == 'r' and c_choice == 's':
         u_score += 1
         print('''You: Rock -- AI: Scissors
-                 YOU WIN''')
+                 YOU WIN
+                  You:{u_score} --- AI:{c_score}''')
 
     if u_choice == 'stop':
         break
